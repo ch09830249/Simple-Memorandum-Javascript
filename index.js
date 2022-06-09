59722e298ef376e0c22f6be9cc83c806ea04e4c0
Kenny Chang <kenny.chang@external.stellantis.com>
2022-06-09 13:03:25 +0800

    Simple memorandum (Using JS)

---
+++ index.js
@@ -0,0 +1,42 @@
+const content = document.getElementById("content")
+const date = document.getElementById("date")
+const time = document.getElementById("time")
+const addedBtn = document.getElementById("addedBtn")
+const deletedBtn = document.getElementById("deletedBtn")
+const list = document.getElementById("list")
+
+const listContent = []
+
+function render(){
+    let htmlStr = ""
+
+    listContent.forEach(function(item){
+        htmlStr = htmlStr + 
+        `
+        <div class="item">
+            <div>
+                <p>內容：${item.content}</p>
+                <p>時間：${item.date} ${item.time}</p>
+            </div>
+        </div>
+        `
+    })
+
+    list.innerHTML = htmlStr
+}
+
+addedBtn.addEventListener('click', function(){
+    listContent.unshift({
+        content: content.value,
+        date: date.value,
+        time: time.value
+    })
+
+    render()
+})
+
+deletedBtn.addEventListener('click', function(){
+    listContent.pop()
+
+    render()
+})
